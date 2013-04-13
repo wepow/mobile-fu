22523a8c70278c16261ad40fa5db2b1e0ac03ce2
Ben Langfeld <ben@langfeld.me>
2013-04-13 15:32:20 -0300

    Clean up checking if device is JS enabled

--- lib/mobile-fu/helper.rb
+++ lib/mobile-fu/helper.rb
@@ -1,7 +1,9 @@
 module MobileFu
   module Helper
+    JS_ENABLED_DEVICES = %w{iphone ipod ipad mobileexplorer android}
+
     def js_enabled_mobile_device?
-      is_device?('iphone') || is_device?('ipod') || is_device?('ipad') || is_device?('mobileexplorer') || is_device?('android')
+      JS_ENABLED_DEVICES.find { |device| is_device? device }
     end
 
     def stylesheet_link_tag_with_mobilization(*sources)
