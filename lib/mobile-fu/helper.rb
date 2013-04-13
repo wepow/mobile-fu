32821b95a83614785a994960c5b1689d6c3137c5
Ben Langfeld <ben@langfeld.me>
2013-04-13 15:32:43 -0300

    Zune haz JS
    Fixes #33

--- lib/mobile-fu/helper.rb
+++ lib/mobile-fu/helper.rb
@@ -1,6 +1,6 @@
 module MobileFu
   module Helper
-    JS_ENABLED_DEVICES = %w{iphone ipod ipad mobileexplorer android}
+    JS_ENABLED_DEVICES = %w{iphone ipod ipad mobileexplorer android zune}
 
     def js_enabled_mobile_device?
       JS_ENABLED_DEVICES.find { |device| is_device? device }
