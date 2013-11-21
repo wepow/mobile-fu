95e801162e52570175f39326f899dea993ebcddd
Darren Cauthon <darren@cauthon.com>
2013-11-20 21:35:58 -0600

    Support for >= 1.9.3 means we can use require_relative.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,4 @@
-require File.expand_path(File.dirname(__FILE__) + '/../lib/mobile-fu')
+require_relative '../lib/mobile-fu'
 require 'minitest/autorun'
 require 'minitest/spec'
 require 'minitest/pride'
