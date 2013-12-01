7edfa3a38336ea407544737d57068a5477dd5ae9
Darren Cauthon <darren@cauthon.com>
2013-12-01 16:37:48 -0600

    Move the user agent code to the tablet module.

--- lib/mobile-fu/tablet.rb
+++ lib/mobile-fu/tablet.rb
@@ -1,9 +1,15 @@
 module MobileFu
+
   module Tablet
 
     def self.user_agents
       /ipad|android 3.0|xoom|sch-i800|gt-p1000|playbook|tablet|kindle|honeycomb|nexus 7|windows nt [0-9.]+; arm/.freeze
     end
 
+    def self.is_a_tablet_device? user_agent
+      !!(user_agent.to_s.downcase =~ ::MobileFu::Tablet.user_agents)
+    end
+
   end
+
 end
